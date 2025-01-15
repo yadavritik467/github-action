9b214e62ebc30e957f698ac2b6c308a565d18cd3
Ritik Yaduvanshi <130385697+yadavritik467@users.noreply.github.com>
2025-01-15 12:02:57 +0530

    Testing (#2)
    * test 4
    
    * create err
    
    ---------
    
    Co-authored-by: ritikgoplus <ritik@go-plus.io>

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
+import { useState } from "react";
 import "./App.css";
 
 function App() {
+  const [d, setD] = useState<string>('')
   return (
     <>
       <p>Click on the Vite and React logos
-         to learn more</p>
+        to learn more</p>
     </>
   );
 }
